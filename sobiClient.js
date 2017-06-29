4313ff5747c989df8eee2dd5943009495403f519
Drewrey Lupton <drewrey@gmail.com>
2017-06-28 23:18:44 -0400

    Get bikes instead of routes

--- sobiClient.js
+++ sobiClient.js
@@ -6,12 +6,14 @@ const options = {
     headers: {
         'Application-Name': 'bikeshare-data',
         'Application-Version': '0.0.1',
+        // TODO: fix this
         'Authorization': 'Bearer ' + config.get('auth_token')
     },
     json: true
 }
 
-rp(_.extend(options, {url: 'https://app.socialbicycles.com/api/routes.json'}))
+rp(_.extend(options, {url: 'https://app.socialbicycles.com/api/bikes.json?network_id=' + config.get('network_id') + '&per_page=1000'}))
+    // TODO: handle this better
     .then( body => {
         console.log(body)
     })
